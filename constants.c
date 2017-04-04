3638ca6927bd4aaa5c01066b17e58f04971106f1
Clifford Richardson <cmrallen@gmail.com>
2017-04-04 00:11:14 -0700

    Add object character representations

--- constants.c
+++ constants.c
@@ -8,3 +8,8 @@ const int GAME_HEIGHT = 10;
 const int GAME_BUFFER_LENGTH = 1000;
 const int GAME_MIN_FLOOR_LENGTH = 3;
 const int GAME_MAX_GAP_LENGTH = 6;
+
+// Object char representations
+const char GAME_PLAYER = '@';
+const char GAME_FLOOR = '=';
+const char GAME_AIR = ' ';
