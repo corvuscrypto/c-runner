9ffe5bf171b46903004f4984b88d633a45928e8d
Clifford Richardson <cmrallen@gmail.com>
2017-04-03 23:55:32 -0700

    First commit of constants file

---
+++ constants.c
@@ -0,0 +1,10 @@
+/*
+ * constants.c
+ *
+ * Defines constants that control the universe! (of the game)
+ */
+
+const int GAME_HEIGHT = 10;
+const int GAME_BUFFER_LENGTH = 1000;
+const int GAME_MIN_FLOOR_LENGTH = 3;
+const int GAME_MAX_GAP_LENGTH = 6;
---
+++ constants.h
@@ -0,0 +1,15 @@
+/*
+ * constants.h
+ *
+ * Header file of our game constants
+ */
+
+#ifndef GAME_CONSTANTS_H
+#define GAME_CONSTANTS_H
+
+extern const int GAME_HEIGHT;
+extern const int GAME_BUFFER_LENGTH;
+extern const int GAME_MIN_FLOOR_LENGTH;
+extern const int GAME_MAX_GAP_LENGTH;
+
+#endif
